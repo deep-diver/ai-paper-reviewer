f36918b31b241ec3b0640cb3f05e9eb5a3ad306a
Chansung Park <deep.diver.csp@gmail.com>
2025-01-18 10:28:35 +0900

    Update manage_dir.sh

--- manage_dir.sh
+++ manage_dir.sh
@@ -3,12 +3,13 @@
 # Get all directories matching YYYY-MM-DD format within ./content/, sorted chronologically
 dirs=$(find ./content -mindepth 1 -maxdepth 1 -type d -name '[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]' | awk -F/ '{print $NF}' | sort -n)
 
-# Get the latest directory (last one in the sorted list)
-latest_dir=$(echo "$dirs" | tail -n 1)
+# Get the 3 latest directories
+latest_dirs=$(echo "$dirs" | tail -n 3)
 
-# Iterate over all directories except the latest one
+# Iterate over all directories
 for dir in $dirs; do
-  if [[ "$dir" != "$latest_dir" ]]; then
+  # Check if the current directory is NOT in the list of latest directories
+  if ! echo "$latest_dirs" | grep -q "$dir"; then
     echo "Processing directory: $dir"
     mv ./content/$dir/* ./content/paper-reviews/
     rm -rf ./content/$dir
